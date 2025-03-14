86c497408a1b072c16a99cd4a8c21741084830b8
Okiki <hey@okikio.dev>
2025-03-14 20:01:06 +0000

    fix: move redirects & rewrites to next.js config
    Signed-off-by: Okiki <hey@okikio.dev>

--- groovybytes-dash/next.config.ts
+++ groovybytes-dash/next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  redirects: async () => [
+    {
+      "source": "/logout",
+      "destination": "/.auth/logout",
+      "permanent": false
+    },
+    {
+      "source": "/login/github",
+      "destination": "/.auth/login/github",
+      "permanent": false
+    }
+  ],
+
+  rewrites: async () => [
+    {
+      "source": "/login",
+      "destination": "/.auth/login/aad"
+    }
+  ]
 };
 
 export default nextConfig;
