346b0d8e49fe758a97ba6617cfc356d12fd37ad8
Okiki Ojo <hey@okikio.dev>
2025-03-30 02:18:52 -0400

    chore: disable standalone next.js

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  basePath: "",
-
   /* config options here */
   eslint: {
     // Warning: This allows production builds to successfully complete even if
