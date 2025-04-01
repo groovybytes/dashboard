f286bba673a5a0d4512cbdd6c494d939a9c13071
Okiki Ojo <hey@okikio.dev>
2025-04-01 02:18:23 -0400

    fix: fix telemtry

--- vendor/appinsights.cjs
+++ vendor/appinsights.cjs
@@ -1,15 +1,28 @@
-let appInsights = require('applicationinsights');
-appInsights
-  .setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
-  .setAutoCollectConsole(true)
-  .setAutoCollectDependencies(true)
-  .setAutoCollectExceptions(true)
-  .setAutoCollectHeartbeat(true)
-  .setAutoCollectPerformance(true, true)
-  .setAutoCollectRequests(true)
-  .setAutoDependencyCorrelation(true)
-  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-  .setSendLiveMetrics(true)
-  .setUseDiskRetryCaching(true);
-appInsights.defaultClient.setAutoPopulateAzureProperties(true);
-appInsights.start();
+   // Import the Azure Monitor OpenTelemetry plugin and OpenTelemetry API
+   const { useAzureMonitor } = require("@azure/monitor-opentelemetry");
+   const { metrics, trace } = require("@opentelemetry/api");
+
+   // Import the OpenTelemetry instrumentation registration function and Express instrumentation
+   const { registerInstrumentations } = require( "@opentelemetry/instrumentation");
+   const {
+     getNodeAutoInstrumentations,
+   } = require('@opentelemetry/auto-instrumentations-node');
+
+   // Get the OpenTelemetry tracer provider and meter provider
+   const tracerProvider = (trace.getTracerProvider()).getDelegate();
+   const meterProvider = metrics.getMeterProvider();
+
+   // Enable Azure Monitor integration
+   useAzureMonitor();
+   
+   // Register the Express instrumentation
+   registerInstrumentations({
+     // List of instrumentations to register
+     instrumentations: [
+      getNodeAutoInstrumentations(),
+     ],
+   // OpenTelemetry tracer provider
+     tracerProvider: tracerProvider,
+     // OpenTelemetry meter provider
+     meterProvider: meterProvider
+   });
